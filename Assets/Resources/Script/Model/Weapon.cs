9273681f8965416c48c9804403a4d14ae426234c
ICT1 <you@example.com>
2024-12-05 16:45:30 +0700

    Menambahkan Fitur Peluru

--- Assets/Resources/Script/Model/Weapon.cs
+++ Assets/Resources/Script/Model/Weapon.cs
@@ -11,14 +11,33 @@ public class Weapon
     public int ammo;
     [field: SerializeField]
     public int damage;
-
-
     [field: SerializeField]
     public float FireRate;
-    public Weapon(int ammo, int damage, float fireRate)
+    [field: SerializeField]
+    public float ShootDelay;
+    [field:SerializeField]
+    public float LastShootTime;
+    
+    public Weapon(int ammo, int damage, float fireRate, float shootDelay, float lastShootTime)
     {
         this.ammo = ammo;
         this.damage = damage;
         FireRate = fireRate;
+        ShootDelay = shootDelay;
+        LastShootTime = lastShootTime;
+    }
+
+    public Vector3 GetDirection(Vector3 pos,bool AddBulletSpread,Vector3 BulletSpreadVariance)
+    {
+        Vector3 direction = pos;
+
+        if (AddBulletSpread)
+        {
+            direction +=new Vector3(Random.Range(-BulletSpreadVariance.x,BulletSpreadVariance.x),Random.Range(-BulletSpreadVariance.y,BulletSpreadVariance.y),Random.Range(-BulletSpreadVariance.z,BulletSpreadVariance.z));
+            direction.Normalize();
+        }
+        return direction;
+       
+       
     }
 }
--- Assets/Resources/Script/View/Weapon_V.cs
+++ Assets/Resources/Script/View/Weapon_V.cs
@@ -2,12 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
 
+
+[RequireComponent(typeof(Animator))]
 public class Weapon_V : MonoBehaviour
 {
-
-  
+    [SerializeField]
+    private bool AddBulletSpread = true;
+    [SerializeField]
+    private Vector3 BulletSpreadVariance = new Vector3(0.1f, 0.1f, 0.1f);
+    [SerializeField]
+    private ParticleSystem ShootingSystem;
+    [SerializeField]
+    private ParticleSystem ImpactParticleSystem;
+    [SerializeField]
+    private TrailRenderer BulletTrail;
+    [SerializeField]
+    private LayerMask Mask;
+    
+    private Animator animator;
+    private float LastShootTIme;
     /// <summary>
     /// Trigger Button
     /// </summary>
@@ -21,8 +37,10 @@ public class Weapon_V : MonoBehaviour
     /// 
     [SerializeField] private GameObject LeftController;
     [SerializeField] private GameObject RightController;
+
     [SerializeField] private GameObject raycast;
     [SerializeField] private GameObject Bullet;
+
     [SerializeField] private GameObject PrefabBullet;
     [SerializeField] private Weapon weapon;
     [SerializeField] private GameObject particle;
@@ -40,7 +58,7 @@ public class Weapon_V : MonoBehaviour
     void Start()
     {
 
-        weapon = new Weapon(weapon.ammo,weapon.damage,weapon.FireRate);
+        weapon = new Weapon(weapon.ammo,weapon.damage,weapon.FireRate,weapon.ShootDelay,weapon.LastShootTime);
         interactor = LeftController.GetComponent<XRDirectInteractor>();
         interactor.selectEntered.AddListener(OnSelectEntered);
         interactor.selectExited.AddListener(OnExitEntered);
@@ -59,16 +77,55 @@ public class Weapon_V : MonoBehaviour
        
     }
     RaycastHit hit;
+    private void Shoot()
+    {
+
+        if (weapon.LastShootTime + weapon.ShootDelay < Time.time)
+        {
+            animator.SetBool("IsShooting", true);
+            ShootingSystem.Play();
+            Vector3 dir = weapon.GetDirection(transform.forward,AddBulletSpread,BulletSpreadVariance);
+            if (Physics.Raycast(raycast.transform.position, dir, out RaycastHit hit, float.MaxValue, Mask))
+            {
+                TrailRenderer trail = Instantiate(BulletTrail, raycast.transform.position, Quaternion.identity);
+                StartCoroutine(SpawnTrail(trail, hit));
+                LastShootTIme = Time.time;
+            }
+        }
+    }
+
+    private IEnumerator SpawnTrail(TrailRenderer Trail, RaycastHit hit)
+    {
+        float time = 0;
+        Vector3 startPosition = Trail.transform.position;
+        while (time < 1)
+        {
+            Trail.transform.position = Vector3.Lerp(startPosition, hit.point, time);
+            time = Time.deltaTime / Trail.time;
+            yield return null;  
+        }
+        animator.SetBool("IsShooting",false);
+        Trail.transform.position = hit.point;
+        Instantiate(ImpactParticleSystem,hit.point,Quaternion.LookRotation(hit.normal));
+        Destroy(Trail.gameObject,Trail.time);
+    }
+
 
     void Update()
     {
+
+        
         Debug.DrawRay(raycast.transform.position, raycast.transform.forward * 10f, Color.red);
         if (Memegang)
         {
             if (LeftTrigger.action.WasPressedThisFrame() || RightTrigger.action.WasPressedThisFrame())
             {
+                Shoot();
+             
+               // weapon.Shoot()
+                //weapon.Shoot(weapon.LastShootTime,weapon.ShootDelay);
 
-                if (Time.time > weapon.FireRate)
+               /* if (Time.time > weapon.FireRate)
                 {
                     ins = Instantiate(PrefabBullet, Bullet.transform.position, Quaternion.identity);
                     audio.Play();
@@ -86,7 +143,7 @@ public class Weapon_V : MonoBehaviour
                     {
                         Debug.Log("Ray did not hit anything.");
                     }
-                }
+                }*/
             }
         }
     }
